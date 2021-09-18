2330f1b7d2619a2805a44495beb794e8080d9a13
Charlotte Lily-Llaitchee Vixen <charlottelilyfields48@gmail.com>
2021-09-18 19:53:04 +0100

    stopped camera script complaining about delta not being used

--- scripts/Camera2D.gd
+++ scripts/Camera2D.gd
@@ -4,7 +4,7 @@ extends Camera2D
 func _ready():
 	pass
 
-func _process(delta):
+func _process(_delta):
 	if Input.is_action_pressed("zoom_in"):
 		zoom = Vector2(zoom.x - 0.01, zoom.y - 0.01)
 	elif Input.is_action_pressed("zoom_out"):
