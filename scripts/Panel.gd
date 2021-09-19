cba67cddff1bc5e26882a3ef64a0846f05da1db5
Charlotte Lily-Llaitchee Vixen <charlottelilyfields48@gmail.com>
2021-09-19 17:49:31 +0100

    fixed debug tracking the screen

--- scripts/Panel.gd
+++ scripts/Panel.gd
@@ -5,5 +5,5 @@ export (NodePath) var TargetPath
 onready var TargetNode = get_node(TargetPath)
 onready var StartOffset = rect_position - TargetNode.position
 
-func _process(delta):
+func _process(_delta):
 	rect_position = TargetNode.position + StartOffset
