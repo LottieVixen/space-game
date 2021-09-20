0f8d8d21b1faf7a23e5ef6743e621ee2eeaca98b
Charlotte Lily-Llaitchee Vixen <charlottelilyfields48@gmail.com>
2021-09-20 12:01:16 +0100

    fixed thrusters! YAY

--- levels/Ship_03.gd
+++ levels/Ship_03.gd
@@ -26,12 +26,12 @@ var strafe_right = 0
 func _draw():
 	#rear thruster
 	draw_circle((thrust_force.rotated(rotation)),5,Color.rebeccapurple)
-	draw_line($Thrusters/Rear.position,($Thrusters/Rear.position + thrust_force.rotated(rotation)*50),Color.red)
+	draw_line($Thrusters/Rear.position.rotated(rotation),($Thrusters/Rear.position.rotated(rotation) + thrust_force.rotated(rotation)*50),Color.red)
 	#draw_line($Thrusters/Rear.position,(thrust_force.rotated($Thrusters/Rear.position.angle_to(position))*50),Color.red)
 	#reverse thrusters
-	draw_circle(($Thrusters/Left/Forward.position + thrust_rev.rotated(rotation)*50),5,Color.rebeccapurple)
+	#draw_circle(($Thrusters/Left/Forward.position + thrust_rev.rotated(rotation)*50),5,Color.rebeccapurple)
 	#get_node("../").add_point( to_global($Thrusters/Left/Forward.global_position + thrust_rev.rotated(rotation)))
-	draw_circle(($Thrusters/Right/Forward.position + thrust_rev.rotated(rotation)*50),5,Color.rebeccapurple)
+	#draw_circle(($Thrusters/Right/Forward.position + thrust_rev.rotated(rotation)*50),5,Color.rebeccapurple)
 	#angular velocity arc
 	draw_arc(Vector2.ZERO, 10, 0, angular_velocity, 32, Color.red, 10, false)
 	
@@ -89,7 +89,8 @@ func _process(_delta):
 
 func _integrate_forces(_state):
 	#apply_impulse($Thrusters/Rear.position, thrust_force.rotated($Thrusters/Rear.position.angle_to(position)) * forward)
-	apply_impulse($Thrusters/Rear.position, thrust_force.rotated(rotation) * forward) #has a slight clockwise spin
+	apply_impulse($Thrusters/Rear.position.rotated(rotation), thrust_force.rotated(rotation) * forward) #has a slight clockwise spin
+	#exert_force($Thrusters/Rear.position, thrust_force * forward)
 	#apply_central_impulse(thrust_force.rotated(rotation) * forward) #works as expected, flies straight at the ship's heading
 	if break_this_time:
 		break_this_time = false
@@ -98,18 +99,21 @@ func _integrate_forces(_state):
 		print("loc: "+ String(self.to_local($Thrusters/Left/Front.global_position)))
 		pass
 	#Reverse
-	apply_impulse($Thrusters/Left/Forward.position.normalized(), thrust_rev.rotated(global_rotation) * reverse)
-	apply_impulse($Thrusters/Right/Forward.position.normalized(), thrust_rev.rotated(global_rotation)  * reverse)
+	apply_impulse($Thrusters/Left/Forward.position.rotated(rotation), thrust_rev.rotated(rotation) * reverse)
+	apply_impulse($Thrusters/Right/Forward.position.rotated(rotation), thrust_rev.rotated(rotation)  * reverse)
 	#Roll Left
-	apply_impulse($Thrusters/Left/Rear.position, thrust_up * roll_left)
-	apply_impulse($Thrusters/Right/Front.position, thrust_down * roll_left)
+	apply_impulse($Thrusters/Left/Rear.position.rotated(rotation), thrust_up.rotated(rotation) * roll_left)
+	apply_impulse($Thrusters/Right/Front.position.rotated(rotation), thrust_down.rotated(rotation) * roll_left)
 	#Roll Right
-	apply_impulse($Thrusters/Left/Front.position, thrust_up * roll_right)
-	apply_impulse($Thrusters/Right/Rear.position, thrust_down * roll_right)
+	apply_impulse($Thrusters/Left/Front.position.rotated(rotation), thrust_up.rotated(rotation) * roll_right)
+	apply_impulse($Thrusters/Right/Rear.position.rotated(rotation), thrust_down.rotated(rotation) * roll_right)
 	#Strafe left
-	apply_impulse($Thrusters/Right/Mid.position.normalized(), thrust_down.rotated(global_rotation) * strafe_left)
+	apply_impulse($Thrusters/Right/Mid.position.rotated(rotation), thrust_down.rotated(rotation) * strafe_left)
 	#Strafe right
-	apply_impulse($Thrusters/Left/Mid.position.normalized(), thrust_up.rotated(global_rotation) * strafe_right)
+	apply_impulse($Thrusters/Left/Mid.position.rotated(rotation), thrust_up.rotated(rotation) * strafe_right)
+
+func exert_force(pos:Vector2,dir:Vector2):
+	apply_impulse(pos.rotated(rotation), dir.rotated(rotation))
 
 func debug():
 	print("local:" + String(to_local($Thrusters/Rear.global_position)))
